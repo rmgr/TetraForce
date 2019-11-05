23ac2bb8bb45c63727193eff990b4527f8903eb7
rmgr <rmgr>
2019-11-05 18:27:54 +1030

    Implemented bomb doing damage to entities. Currently works as a trap. May create a new 'EXPLOSIVES' type and make it that.

--- items/bomb.gd
+++ items/bomb.gd
@@ -3,17 +3,21 @@ extends Item
 
 var shooter
 var timer
+var exploded
+var affectedBodies
 puppet var puppet_pos = position
 
 func start():
-	
+	exploded = false
+	DAMAGE *= 4
+	TYPE = "TRAP"
 	shooter = get_parent()
 	add_to_group("projectile")
 	z_index = shooter.z_index - 1
 	
 	get_parent().remove_child(self)
 	shooter.get_parent().add_child(self)
-	
+	#$Hitbox.connect("body_entered", self, "body_entered")
 	position = shooter.position
 	timer = Timer.new()
 	timer.connect("timeout", self, "_on_timeout")
@@ -25,13 +29,19 @@ func start():
 func _physics_process(delta):
 	return
 
-func body_entered(body):
-	if body.get("TYPE") != TYPE:
-		delete()
 
 func _on_timeout():
+	timer.stop()
+	$Hitbox/CollisionShape2D.disabled = false
 	print_debug("boom")
-	delete()
+	exploded = true
+	var timer2 = Timer.new()
+	timer2.connect("timeout", self, "delete")
+	timer2.set_wait_time( 1 )
+	add_child(timer2)
+	timer2.start()
+	
+	
 
 sync func delete():
 	queue_free()
