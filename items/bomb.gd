9ea705f0511486149989fe4ca2c35a1c4f9433cb
rmgr <rmgr>
2019-11-10 12:54:19 +1030

    Code cleanup/commenting ahead of PR

--- items/bomb.gd
+++ items/bomb.gd
@@ -1,57 +1,71 @@
 extends Item
 
-
 var shooter
 var timer
 var exploded
 var blasted_walls
+var fuse
 
 puppet var puppet_pos = position
 
 func start():
-	TYPE = "TRAP"
-	DAMAGE = 0
-	blasted_walls = []
 	shooter = get_parent()
-	add_to_group("projectile")
-	z_index = shooter.z_index + 1
+	blasted_walls = []
+	fuse = 2
 	exploded = false
-	get_parent().remove_child(self)
-	shooter.get_parent().add_child(self)
+	TYPE = "TRAP"
+	add_to_group("projectile")
+	
+	#Set damage to 0
+	DAMAGE = 0
+	
+	set_physics_process(true)
+	$Hitbox.connect("body_entered", self, "body_entered")
+	
+	#set up positioning data
 	position = shooter.position
+	z_index = shooter.z_index + 1
+	
+	#move bomb from player object to world.
+	shooter.remove_child(self)
+	shooter.get_parent().add_child(self)
+	
+	#Set up timer
 	timer = Timer.new()
 	timer.connect("timeout", self, "_on_timeout")
 	timer.set_wait_time( 2 )
 	add_child(timer)
 	timer.start()
-	set_physics_process(true)
-	
-	$Hitbox.connect("body_entered", self, "body_entered")
 		
-
-func _physics_process(delta: float) -> void:
-	pass
-	
 func body_entered(body):
-	if !exploded:
-		blasted_walls.push_back(body)
-	
+	#Add all blastable walls to an array for later.
+	if body.has_method("blast"):
+		if !exploded:
+			blasted_walls.push_back(body)
 
 func _on_timeout():
 	timer.stop()
 	exploded = true
+	# We're exploding now, set the damage value.
 	DAMAGE = 2
+	
+	# iterate over blasted walls and trigger the blast method
 	for body in blasted_walls:
 		if body.has_method("blast"):
 			body.blast()
+			
+	#attach the explode animation
 	var death_animation = preload("res://enemies/enemy_death.tscn").instance()
 	death_animation.global_position = global_position
 	get_parent().add_child(death_animation)
+	
+	# hide the sprite
 	$Sprite.visible = false
-	print_debug("boom - play animation here")
+	
+	#set a timer to delete the bomb in a half second. 
 	var timer2 = Timer.new()
 	timer2.connect("timeout", self, "delete")
-	timer2.set_wait_time( 0.25 )
+	timer2.set_wait_time( 0.5 )
 	add_child(timer2)
 	timer2.start()
 	
--- tiles/blastable_wall.gd
+++ tiles/blastable_wall.gd
@@ -1,31 +1,35 @@
 tool
 extends StaticBody2D
 
-
 export var is_blasted = false
 export var patchdirection = ""
 var tiles_to_patch
-var all_set = false
+var all_set
 var parent 
+
 func _ready() -> void:
+	#Set up patch dictionary and globals
 	tiles_to_patch = {"left": -1, "right" : -1, "up" : -1, "down" : -1}
-	set_process(true)
-	set_physics_process(true)
 	is_blasted = false
 	visible = true
+	all_set = false
+	parent = get_parent()
+	
+	#set up collision and physics
+	set_physics_process(true)
 	set_collision_layer_bit(0,1)
 	set_collision_layer_bit(1,1)
-	parent = get_parent()
-	connect("body_entered", self, "body_entered")
-	var l = load("res://tiled/overworld/overworld_walls.tsx")
-	
 
 func _physics_process(delta):
 	if !all_set:
 		if parent != null:
+			#Find current tile in teh tilemap
 			var wall_tilemap = parent.get_node("walls")
 			var tile_pos = wall_tilemap.world_to_map(position)
 			var this_tile = wall_tilemap.get_cellv(tile_pos)
+			
+			#Work out directions to patch and save them to a
+			#dictionary for later.
 			if ("l" in patchdirection):
 				var left_tile = Vector2(tile_pos.x - 1, tile_pos.y)
 				tiles_to_patch["left"] = wall_tilemap.get_cellv(left_tile)
@@ -45,16 +49,25 @@ func _physics_process(delta):
 				var down_tile = Vector2(tile_pos.x, tile_pos.y + 1)
 				tiles_to_patch["down"] = wall_tilemap.get_cellv(down_tile)
 				wall_tilemap.set_cellv(down_tile, this_tile)
-			
+				
+			#We only want to do this once.
 			all_set = true
 	
 func blast():
+	#set flags
 	is_blasted = true
-	# Do fancy animation or something, then make door inactive
+	visible = false
+	set_collision_layer_bit(0,0)
+	set_collision_layer_bit(1,0)
+	
+	#find tilemap
 	var wall_tilemap = parent.get_node("walls")
 	var tile_pos = wall_tilemap.world_to_map(position)
+	
+	#hide the broken wall
 	wall_tilemap.set_cellv(tile_pos, -1)
 	
+	#overwrite saved wall tiles from the dictionary we set up earlier.
 	if ("l" in patchdirection):
 		var left_tile = Vector2(tile_pos.x - 1, tile_pos.y)
 		wall_tilemap.set_cellv(left_tile, tiles_to_patch["left"] )
@@ -71,6 +84,3 @@ func blast():
 		var down_tile = Vector2(tile_pos.x , tile_pos.y + 1)
 		wall_tilemap.set_cellv(down_tile, tiles_to_patch["down"])
 	
-	visible = false
-	set_collision_layer_bit(0,0)
-	set_collision_layer_bit(1,0)
